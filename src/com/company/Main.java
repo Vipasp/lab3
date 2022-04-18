258d2cf1ed15810d13bd048267c1a867e4c6b2d1
Vipasp <vipasp@yandex.ru>
2022-04-18 18:59:09 +0300

    Лаб раб 3_Конопелько О.Н.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1234");
+
+            int array[] = new int[] {2,3,4,5};
+            int proizv = 1;
+            for (int i = 0; i < array.length; i++) {
+                proizv=array[i] * proizv;
+            }
+            System.out.println("Произведение элементов массива = " + proizv);
+
+
 
 
     }
